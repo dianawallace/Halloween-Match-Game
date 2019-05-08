ba98778d489aa74ba2bcf416fdebe98646943cf7
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-05-08 03:27:38 +0000

    added ready function for overlay, start-game, and flip-cards

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+
+
+
+function ready() {
+  let overlays = Array.from(document.getElementsByClassName("overlay-text"));
+  let cards = Array.from(document.getElementsByClassName("card"));
+  let game = new MixOrMatch(100, cards);
+
+  overlays.forEach(overlay => {
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("visible");
+      game.startGame();
+      let audioController = new AudioController();
+      audioController.startMusic();
+    });
+  });
+  cards.forEach(card => {
+    card.addEventListener("click", () => {
+      game.flipCard(card);
+    });
+  });
+}
+
+
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", ready());
 } else {
