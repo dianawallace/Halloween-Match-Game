613db1c9f58678999f8dbc236173ec0659500f35
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-05-10 05:13:01 +0000

    added AudioController class constructor

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-
+class AudioController {
+  constructor() {
+    this.bgMusic = new Audio("Assets/Audio/creepy-background-daniel_simon.mp3");
+    this.flipSound = new Audio("Assets/Audio/cardTakeOutPackage2.wav");
+    this.matchSound = new Audio("Assets/Audio/Jingle_Achievement_01.mp3");
+    this.victorySound = new Audio("Assets/Audio/Jingle_Win_00.mp3");
+    this.gameOverSound = new Audio("Assets/Audio/game_over.mp3");
+    this.bgMusic.volume = 0.5;
+    this.bgMusic.loop = true;
+  }
 
 
 function ready() {
